8c05f7691bb79e858d95d8556e224de309a19485
Ayroid <ayushsk0000@gmail.com>
2024-09-09 18:25:52 +0530

    feat: create user model

---
+++ models.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+from enum import Enum
+
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+    other = "other"
+
+
+class Role(str, Enum):
+    user = "user"
+    admin = "admin"
+    student = "student"
+
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4()
+    first_name: str
+    last_name: str
+    middle_name: Optional[str]
+    gender: Gender
+    roles: List[Role]
